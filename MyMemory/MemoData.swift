b19fe9b821f9d3466c15e4e06d83f0d621af1051
sobamemil <sieh96@naver.com>
2021-01-19 09:19:41 +0900

    data structure of memo application

---
+++ MyMemory/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by 심찬영 on 2021/01/18.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx: Int? // 데이터 식별값
+    var title: String? // 메모 제목
+    var contents: String? // 메모 내용
+    var image: UIImage? // 이미지
+    var regdate: Date? // 작성일
+}
