248e481b6bba301c15dd08709c629e7aed1929f8
sobamemil <sieh96@naver.com>
2021-01-24 16:11:12 +0900

    메모장 구현 - 계정 관리 상세 화면에 배경 이미지 삽입

--- MyMemory/ProfileVC.swift
+++ MyMemory/ProfileVC.swift
@@ -17,13 +17,25 @@ class ProfileVc: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let backBtn = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(close(_:)))
         self.navigationItem.leftBarButtonItem = backBtn
         
+        // 배경 이미지 생성
+        let bg = UIImage(named: "profile-bg.png")
+        let bgImg = UIImageView(image: bg)
+        
+        bgImg.frame.size = CGSize(width: bgImg.frame.size.width, height: bgImg.frame.size.height)
+        bgImg.center = CGPoint(x: self.view.frame.size.width / 2, y: 40)
+        
+        bgImg.layer.cornerRadius = bgImg.frame.size.width / 2
+        bgImg.layer.borderWidth = 0
+        bgImg.layer.masksToBounds = true
+        self.view.addSubview(bgImg)
+        
         // 프로필 사진에 들어갈 기본 이미지
         let image = UIImage(named:  "account.jpg")
         
         // 프로필 이미지 처리
         self.profileImage.image = image
         self.profileImage.frame.size = CGSize(width: 100, height: 100)
-        self.profileImage.center = CGPoint(x: self.view.frame.size.width / 2, y: 160)
+        self.profileImage.center = CGPoint(x: self.view.frame.size.width / 2, y: 270)
         
         // 프로필 이미지 둥글게 만들기
         self.profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2
@@ -39,6 +51,9 @@ class ProfileVc: UIViewController, UITableViewDelegate, UITableViewDataSource {
         self.tv.delegate = self
         
         self.view.addSubview(self.tv)
+        
+        // 내비게이션 바 숨김 처리
+        self.navigationController?.navigationBar.isHidden = true
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
