a8c1f82d8da2b4b35c5a056c7e016a0e11e0663e
sobamemil <sieh96@naver.com>
2021-01-25 18:04:02 +0900

    메모장 구현 - 프로퍼티 리스트 적용 중

---
+++ MyMemory/UserInfoManager.swift
@@ -0,0 +1,71 @@
+//
+//  UserInfoManager.swift
+//  MyMemory
+//
+//  Created by 심찬영 on 2021/01/25.
+//
+
+import UIKit
+
+struct UserInfoKey {
+    // 저장에 사용할 키
+    static let loginId = "LOGINID"
+    static let account = "ACCOUNT"
+    static let name = "NAME"
+    static let profile = "PROFILE"
+}
+
+// 계정 및 사용자 정보를 저장 관리하는 클래스
+class UserInfoManager {
+    // 연산 프로퍼티 loginId 정의
+    var loginId: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: UserInfoKey.loginId)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.loginId)
+            ud.synchronize()
+        }
+    }
+    
+    var account: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.account)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.account)
+            ud.synchronize()
+        }
+    }
+    
+    var name: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.name)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.name)
+            ud.synchronize()
+        }
+    }
+    
+    var profile: UIImage? {
+        get {
+            let ud = UserDefaults.standard
+            if let _profile = ud.data(forKey: UserInfoKey.profile) {
+                return UIImage(data: _profile)
+            } else {
+                return UIImage(named: "account.jpg") // 이미지가 없다면 기본 이미지로
+            }
+        }
+        set(v) {
+            if v != nil {
+                let ud = UserDefaults.standard
+                ud.set(v!.pngData(), forKey: UserInfoKey.profile)
+                ud.synchronize()
+            }
+        }
+    }
+}
