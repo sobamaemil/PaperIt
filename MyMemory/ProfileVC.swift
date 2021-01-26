b0be6b1b68d5a8ea1e047ca17a2b5ec16051b6b1
sobamemil <sieh96@naver.com>
2021-01-26 15:38:50 +0900

    메모장 구현 - 프로퍼티 리스트 적용하여 로그인, 로그아웃 구현

--- MyMemory/ProfileVC.swift
+++ MyMemory/ProfileVC.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 class ProfileVc: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let uinfo = UserInfoManager() // 개인 정보 관리 매니저
+    
     let profileImage = UIImageView() // 프로필 사진 이미지
     let tv = UITableView() // 프로필 목록
     
@@ -30,7 +32,8 @@ class ProfileVc: UIViewController, UITableViewDelegate, UITableViewDataSource {
         self.view.addSubview(bgImg)
         
         // 프로필 사진에 들어갈 기본 이미지
-        let image = UIImage(named:  "account.jpg")
+//        let image = UIImage(named:  "account.jpg")
+        let image = self.uinfo.profile
         
         // 프로필 이미지 처리
         self.profileImage.image = image
@@ -54,6 +57,10 @@ class ProfileVc: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         // 내비게이션 바 숨김 처리
         self.navigationController?.navigationBar.isHidden = true
+        
+        // 최초 화면 로딩 시 로그인 상태에 따라 적절히 로그인/로그아웃 버튼을 출력
+        self.drawBtn()
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -72,17 +79,104 @@ class ProfileVc: UIViewController, UITableViewDelegate, UITableViewDataSource {
         switch indexPath.row {
             case 0 :
                 cell.textLabel?.text = "이름"
-                cell.detailTextLabel?.text = "심찬영"
+//                cell.detailTextLabel?.text = "심찬영"
+                cell.detailTextLabel?.text = self.uinfo.name ?? "Login Please"
             case 1 :
                 cell.textLabel?.text = "계정"
-                cell.detailTextLabel?.text = "sieh96@naver.com"
-        default :
-            ()
+//                cell.detailTextLabel?.text = "sieh96@naver.com"
+                cell.detailTextLabel?.text = self.uinfo.account ?? "Login Please"
+            default :
+                ()
         }
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if self.uinfo.isLogin == false {
+            // 로그인이 되어있지 않다면 로그인 창을 띄워줌
+            self.doLogin(self.tv)
+        }
+    }
+    
     @objc func close(_ sender: UIButton) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func doLogin(_ sender: Any) {
+        let loginAlert = UIAlertController(title: "LOGIN", message: nil, preferredStyle: .alert)
+        
+        // 알림창에 들어갈 입력폼 추가
+        loginAlert.addTextField(configurationHandler: { (tf) in
+            tf.placeholder = "Your Account"
+        })
+        
+        loginAlert.addTextField(configurationHandler: { (tf) in
+            tf.placeholder = "Password"
+            tf.isSecureTextEntry = true
+        })
+        
+        // 알림창 버튼 추가
+        loginAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        loginAlert.addAction(UIAlertAction(title: "Login", style: .destructive, handler: { (_) in
+            let account = loginAlert.textFields?[0].text ?? "" // 첫 번째 필드 : 계정
+            let passwd = loginAlert.textFields?[1].text ?? "" // 두 번째 필드 : 비밀번호
+            
+            if self.uinfo.login(account: account, passwd: passwd) {
+                // 로그인 성공 시 처리할 내용
+                self.tv.reloadData() // 테이블 뷰를 갱신
+                self.profileImage.image = self.uinfo.profile // 이미지 프로필을 갱신
+                self.drawBtn() // 로그인 상태에 따라 적절히 로그인/로그아웃 버튼을 출력
+            }  else {
+                let msg = "로그인에 실패하였습니다."
+                let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alert, animated: false, completion: nil)
+            }
+        }))
+        self.present(loginAlert, animated: false, completion: nil)
+    }
+    
+    @objc func doLogout(_ sender: Any) {
+        let msg = "로그아웃하시겠습니까?"
+        let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "확인", style: .destructive, handler: { (_) in
+            if self.uinfo.logout() {
+                // 로그아웃 시 처리할 내용
+                self.tv.reloadData() // 테이블 뷰를 갱신
+                self.profileImage.image = self.uinfo.profile // 이미지 프로필을 갱신
+                self.drawBtn() // 로그인 상태에 따라 적절히 로그인/로그아웃 버튼을 출력
+            }
+        }))
+        self.present(alert, animated: false)
+    }
+
+    func drawBtn() {
+        // 버튼을 감쌀 뷰를 정의
+        let v = UIView()
+        v.frame.size.width = self.view.frame.size.width
+        v.frame.size.height = 40
+        v.frame.origin.x = 0
+        v.frame.origin.y = self.tv.frame.origin.y + self.tv.frame.height
+        v.backgroundColor = UIColor.darkGray
+        
+        self.view.addSubview(v)
+        
+        // 버튼을 정의
+        let btn = UIButton(type: .system)
+        btn.frame.size.width = 100
+        btn.frame.size.height = 30
+        btn.center.x = v.frame.size.width / 2
+        btn.center.y = v.frame.size.height / 2
+        
+        // 로그인 상태일 때는 로그아웃 버튼을, 로그인 상태일 때는 로그아웃 버튼을 만들어 줌
+        if self.uinfo.isLogin == true {
+            btn.setTitle("로그아웃", for: .normal)
+            btn.addTarget(self, action: #selector(doLogout(_:)), for: .touchUpInside)
+        } else {
+            btn.setTitle("로그인", for: .normal)
+            btn.addTarget(self, action: #selector(doLogin(_:)), for: .touchUpInside)
+        }
+        v.addSubview(btn)
     }
 }
