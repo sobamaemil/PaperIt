9e70cc4d475c0b6bbd55974e739ef232ab1a53d3
sobamemil <sieh96@naver.com>
2021-01-26 17:28:24 +0900

    메모장 구현 - 튜토리얼 화면 구현 중

---
+++ MyMemory/TutorialMasterVC.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialMasterVC.swift
+//  MyMemory
+//
+//  Created by 심찬영 on 2021/01/26.
+//
+
+import UIKit
+class TutorialMasterVC: UIViewController {
+    var pageVC: UIViewController!
+    
+    // 콘텐츠 뷰 컨트롤러에 들어갈 타이틀과 이미지
+    var contentTitles = ["STEP 1", "STEP 2", "STEP 3", "STEP 4"]
+    var contentImages = ["Page0", "Page1", "Page2", "Page3"]
+    
+    
+}
