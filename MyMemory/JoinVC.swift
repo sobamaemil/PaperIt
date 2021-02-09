83d107012e90b7e5935b405b4e43d89308a1e0fd
sobamemil <sieh96@naver.com>
2021-02-09 19:47:47 +0900

    메모장 구현 - 계정 등록 기능 구현중 (스토리보드 구현 및 기본 작업)

---
+++ MyMemory/JoinVC.swift
@@ -0,0 +1,18 @@
+//
+//  JoinVC.swift
+//  MyMemory
+//
+//  Created by 심찬영 on 2021/02/09.
+//
+
+import UIKit
+import Alamofire
+
+class JoinVC: UIViewController {
+    @IBOutlet weak var tableVIew: UITableView!
+    @IBOutlet weak var profile: UIImageView!
+    
+    @IBAction func submit(_ sender: Any) {
+        
+    }
+}
