88fb99a99c5ca15219d494e3be41d68933bd3074
sobamemil <sieh96@naver.com>
2021-02-10 14:21:11 +0900

    메모장 구현 - 입력 필드로 이루어진 테이블 뷰 구현

--- MyMemory/JoinVC.swift
+++ MyMemory/JoinVC.swift
@@ -8,11 +8,59 @@
 import UIKit
 import Alamofire
 
-class JoinVC: UIViewController {
-    @IBOutlet weak var tableVIew: UITableView!
+class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var profile: UIImageView!
     
     @IBAction func submit(_ sender: Any) {
         
+    }
+    
+    // 테이블 뷰에 들어갈 텍스트 필드들
+    var fieldAccount: UITextField! // 계정 필드
+    var fieldPassword: UITextField! // 비밀번호 필드
+    var fieldName: UITextField! // 이름 필드
+    
+    override func viewDidLoad() {
+        // 테이블 뷰의 dataSource, delegate 속성 지정
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        // 각 테이블 뷰 셀 모두에 공통으로 적용될 프레임 객체
+        let tfFrame = CGRect(x: 20, y: 0, width: cell.bounds.width - 20, height: 37)
+        switch indexPath.row {
+        case 0:
+            self.fieldAccount = UITextField(frame: tfFrame)
+            self.fieldAccount.placeholder = "계정(이메일)"
+            self.fieldAccount.borderStyle = .none
+            self.fieldAccount.autocapitalizationType = .none
+            self.fieldAccount.font = UIFont.systemFont(ofSize: 14)
+            cell.addSubview(self.fieldAccount)
+        case 1:
+            self.fieldPassword = UITextField(frame: tfFrame)
+            self.fieldPassword.placeholder = "비밀번호"
+            self.fieldPassword.borderStyle = .none
+            self.fieldPassword.autocapitalizationType = .none
+            self.fieldPassword.font = UIFont.systemFont(ofSize: 14)
+            cell.addSubview(self.fieldPassword)
+        case 2:
+            self.fieldName = UITextField(frame: tfFrame)
+            self.fieldName.placeholder = "이름"
+            self.fieldName.borderStyle = .none
+            self.fieldName.autocapitalizationType = .none
+            self.fieldName.font = UIFont.systemFont(ofSize: 14)
+            cell.addSubview(self.fieldName)
+        default:
+            ()
+        }
+        return cell
     }
 }
