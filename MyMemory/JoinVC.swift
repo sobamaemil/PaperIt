c3211a2c8fb1c9f35220b55a650dee2b74b17bc3
sobamemil <sieh96@naver.com>
2021-02-10 14:36:31 +0900

    프로필 이미지 설정 기능 구현

--- MyMemory/JoinVC.swift
+++ MyMemory/JoinVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Alamofire
 
-class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var profile: UIImageView!
     
@@ -25,6 +25,50 @@ class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         // 테이블 뷰의 dataSource, delegate 속성 지정
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        // 프로필 이미지를 원형으로 설정
+        self.profile.layer.cornerRadius = self.profile.frame.width / 2
+        self.profile.layer.masksToBounds = true
+        
+        // 프로필 이미지에 탭 제스처 및 액션 이벤트 설정
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tappedProfile(_:)))
+        self.profile.addGestureRecognizer(gesture)
+    }
+    
+    @objc func tappedProfile(_ sender: Any) {
+        // 전반부) 원하는 소스 타입을 선택할 수 있는 액션 시트 구현
+        let msg = "프로필 이미지를 읽어올 곳을 선택하세요."
+        let sheet = UIAlertController(title: msg, message: nil, preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "포토 라이브러리", style: .default, handler: { _ in
+            selectLibrary(src: .photoLibrary) // 포토 라이브러리에서 이미지 선택
+        }))
+        sheet.addAction(UIAlertAction(title: "카메라", style: .default, handler: { _ in
+            selectLibrary(src: .camera) // 카메라에서 이미지 촬영
+        }))
+        self.present(sheet, animated: false, completion: nil)
+        
+        // 후반부) 전달된 소스 타입에 맞게 이미지 피커 창을 여는 내부 함수
+        func selectLibrary(src: UIImagePickerController.SourceType) {
+            if UIImagePickerController.isSourceTypeAvailable(src) {
+                let picker = UIImagePickerController()
+                picker.allowsEditing = true
+                picker.delegate = self
+                
+                self.present(picker, animated: false, completion: nil)
+            } else {
+                self.alert("사용할 수 없는 타입입니다.")
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let rawVal = UIImagePickerController.InfoKey.editedImage.rawValue
+        if let img = info[UIImagePickerController.InfoKey(rawValue: rawVal)] as? UIImage {
+            self.profile.image = img
+        }
+        self.dismiss(animated: true, completion: nil)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
