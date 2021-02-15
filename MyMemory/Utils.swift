abbdbd8cdbcf3478a7a89c4ea4f8af1a7101529c
sobamemil <sieh96@naver.com>
2021-02-15 16:57:24 +0900

    메모장 구현 - 키 체인에 저장된 값을 읽어오는 load 메소드 정의

--- MyMemory/Utils.swift
+++ MyMemory/Utils.swift
@@ -27,6 +27,32 @@ class TokenUtils {
         assert(status == noErr, "토큰 값 저장에 실패했습니다.")
         NSLog("status=\(status)")
     }
+    
+    // 키 체인에 저장된 값을 읽어오는 메소드
+    func load(_ service: String, account: String) -> String? {
+        // 키 체인 쿼리 정의
+        let keyChainQuery: NSDictionary = [
+            kSecClass : kSecClassGenericPassword,
+            kSecAttrService : service,
+            kSecAttrAccount : account,
+            kSecReturnData : kCFBooleanTrue!, // CFDataRef
+            kSecMatchLimit : kSecMatchLimitOne
+        ]
+        
+        // 키 체인에 저장된 값을 읽어옴
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(keyChainQuery, &dataTypeRef)
+        
+        // 처리 결과가 성공이라면 읽어온 값을 Data 타입으로 변환하고, 다시 String 타입으로 변환
+        if (status == errSecSuccess) {
+            let retrivedData = dataTypeRef as! Data
+            let value = String(data: retrivedData, encoding: .utf8)
+            return value
+        } else { // 처리 결과가 실패라면 nil을 반환
+            print("Nothing was retrieved from the keychain. Status code \(status)")
+            return nil
+        }
+    }
 }
 
 extension UIViewController {
