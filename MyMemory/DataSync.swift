14848a63912bf75e83129b4358ac236345328266
sobamemil <sieh96@naver.com>
2021-02-23 13:32:44 +0900

    메모장 구현 - 서버에 백업된 데이터를 내려받기 위해 sownloadBackupData 메소드 구현 중

--- MyMemory/DataSync.swift
+++ MyMemory/DataSync.swift
@@ -15,6 +15,28 @@ class DataSync {
         let appDalegate = UIApplication.shared.delegate as! AppDelegate
         return appDalegate.persistentContainer.viewContext
     }()
+    
+    // 서버에 백업된 데이터 내려받기
+    func downloadBackupData() {
+        // 최초 한 번만 다운로드 받도록 체크
+        let ud = UserDefaults.standard
+        guard ud.value(forKey: "firstLogin") == nil else {
+            return
+        }
+        
+        // API 호출용 인증 헤더
+        let tk = TokenUtils()
+        let header = tk.getAuthorizationHeader()
+        
+        // API 호출
+        let url = "http://swiftapi.rubypaper.co.kr:2029/memo/search"
+        let get = AF.request(url, method: .post, encoding: JSONEncoding.default, headers: header)
+        
+        // 응답 처리
+        get.responseJSON { res in
+            
+        }
+    }
 }
 
 // MARK: - DataSync 유틸 메소드
