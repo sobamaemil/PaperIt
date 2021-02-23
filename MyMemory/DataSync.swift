298d93bb0b7ee310103cbc79da213dd4cb31e005
sobamemil <sieh96@naver.com>
2021-02-23 14:02:13 +0900

    메모장 구현 - downloadBackupData 메소드의 API 응답 처리 부분 작성

--- MyMemory/DataSync.swift
+++ MyMemory/DataSync.swift
@@ -34,7 +34,40 @@ class DataSync {
         
         // 응답 처리
         get.responseJSON { res in
+            // 응답 결과가 잘못되었거나 list 항목이 없을 경우에는 잘못된 응답이므로 그대로 종료
+            guard let jsonObject = try! res.result.get() as? NSDictionary else { return }
+            guard let list = jsonObject["list"] as? NSArray else { return }
             
+            // list 항목을 순회하면서 각각의 데이터를 코어 데이터에 저장
+            for item in list {
+                guard let record = item as? NSDictionary else {
+                    return
+                }
+                
+                // MemoMO 타입의 관리 객체 인스턴스를 생성하고, 각 속성에 값을 대입
+                let object = NSEntityDescription.insertNewObject(forEntityName: "Memo", into: self.context) as! MemoMO
+                object.title = (record["title"] as! String)
+                object.contents = (record["contents"] as! String)
+                object.regdate = self.stringToDate(record["create_date"] as! String)
+                object.sync = true
+                
+                // 이미지가 있을 경우에만 대입
+                if let imagePath = record["image_path"] as? String {
+                    let url = URL(string: imagePath)!
+                    object.image = try! Data(contentsOf: url)
+                }
+            } // for item in list { ...
+            
+            // 영구 저장소에 커밋
+            do {
+                try self.context.save()
+            } catch let e as NSError {
+                self.context.rollback()
+                NSLog("An error has occurred : %s", e.localizedDescription)
+            }
+            
+            // 다운로드가 끝났으므로 이후로는 실행되지 않도록 처리
+            ud.setValue(true, forKey: "firstLogin")
         }
     }
 }
